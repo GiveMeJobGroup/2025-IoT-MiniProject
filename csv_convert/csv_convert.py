8f093991179fa7c9bb8dc69596793ffc267e2b8c
hyeonnnho <tiger1330@naver.com>
2025-03-28 15:32:54 +0900

    csv 업데이트
    컬럼 값 채우기(D, R, D+R)
    D+R 120이상 일때 1

--- csv_convert/csv_convert.py
+++ csv_convert/csv_convert.py
@@ -1,7 +1,11 @@
 import os
 import pandas as pd
 
+<<<<<<< Updated upstream
 sleep = pd.DataFrame(columns = ['ID'] + list(range(1, 181)))
+=======
+sleep = pd.DataFrame(columns = ['ID'] + list(range(1, 102)) + ['D', 'R', 'D+R', 'FS'])
+>>>>>>> Stashed changes
 
 address = "csv_convert/data/labels"
 path = os.listdir(address) # 데이터 파일 리스트
@@ -14,17 +18,38 @@ for i in range(31):
     file = pd.read_csv(file_path, sep=' ', names=['time', 'sleep'], header=None) # 텍스트 파일 csv 형태로 읽어오기
     
     stage = file['sleep'][0] # 수면단계 변화 관찰하기 위한 첫번째 단계
-    col_index = 1
     
+    col_index = 1
+    countR = 0
+    countD = 0   
+
     for size in range(len(file)):
+        if file.loc[size, 'sleep'] == 2:
+            file.loc[size, 'sleep'] = 1
+    
         s = file['sleep'][size]
         if stage != s:      # 수면단계가 다를 때
-            if col_index <= 180:
+            if col_index <= 101:
                 sleep.iloc[i, col_index] = stage
                 col_index += 1
             stage = s
+        if file.loc[size, 'sleep'] == 3:
+            countD += 1
+        elif file.loc[size, 'sleep'] == 5:
+            countR += 1
+        
+    D = (countD*30)/60
+    R = (countR*30)/60
+    sleep.loc[i, 'D'] = D
+    sleep.loc[i, 'R'] = R
 
-print(sleep)
+    sum = D + R
+    if sum >= 120:
+        sleep.loc[i, 'D+R'] = 1
+    else:
+        sleep.loc[i, 'D+R'] = 0
+
+# print(sleep)
 
 save_path = "csv_convert/sleep_data.csv"
 sleep.to_csv(save_path, index=False, encoding='utf-8')  # 한글 깨짐 방지
